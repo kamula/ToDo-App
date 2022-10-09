b01969798a80b00eb306e20e7bf76d8ae1050ece
Kamula <kamulaisaac@gmail.com>
2022-10-09 11:52:48 +0300

    account registration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     '''User registration view'''
-    return render(request, 'accounts/Register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)        
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'Account Created successfully')
+            return redirect('login_view')
+        else:
+            messages.add_message(request,messages.WARNING,form.error_messages)
+            return redirect('register_view')
+    else:
+        form = UserCreationForm()
+        context = {
+            'form':form
+        }        
+    return render(request, 'accounts/Register.html',context)
 
 
 def login(request):
