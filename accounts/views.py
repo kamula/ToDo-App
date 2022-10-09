9a96ae15114f589d6c526b7af7a3e9104b3b7a23
Kamula <kamulaisaac@gmail.com>
2022-10-09 12:43:13 +0300

    refactor code

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,27 @@
-from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
 
 def register(request):
     '''User registration view'''
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)        
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.add_message(request,messages.SUCCESS,'Account Created successfully')
+            messages.add_message(request, messages.SUCCESS,
+                                 'Account Created successfully')
             return redirect('login_view')
         else:
-            messages.add_message(request,messages.WARNING,form.error_messages)
+            messages.add_message(
+                request, messages.WARNING, form.error_messages)
             return redirect('register_view')
     else:
         form = UserCreationForm()
         context = {
-            'form':form
-        }        
-    return render(request, 'accounts/Register.html',context)
+            'form': form
+        }
+    return render(request, 'accounts/Register.html', context)
 
 
-def login(request):
-    '''Login View'''
-    return render(request, 'accounts/Login.html')
