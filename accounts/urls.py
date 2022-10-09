c8ddf73f5979846ca1f15b21103b406af15f5205
Kamula <kamulaisaac@gmail.com>
2022-10-09 12:40:44 +0300

    login and logout routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register',views.register,name='register_view'),
-    path('login',views.login,name='login_view'),
+    path('register', views.register, name='register_view'),
+    path('login', auth_views.LoginView.as_view(template_name = 'accounts/Login.html'), name='login_view'),
+    path('logout', auth_views.LogoutView.as_view(template_name = 'accounts/Logout.html'), name='logout_view'),
 ]
