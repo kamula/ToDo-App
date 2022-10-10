a915041918ae642e7d209df972923c701ef4f5e7
Kamula <kamulaisaac@gmail.com>
2022-10-10 08:13:37 +0300

    rename

--- todo/forms.py
+++ todo/forms.py
@@ -2,13 +2,13 @@ from django import forms
 from . models import Category, Todo
 
 
-class AddCategoryForm(forms.ModelForm):
+class CreateCategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name', 'created_by']
 
 
-class AddTodoForm(forms.ModelForm):
+class CreateTodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'description', 'scheduled_time']
