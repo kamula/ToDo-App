8382e33b1ea10f61f66ab6738c5fcd0b6397586c
Kamula <kamulaisaac@gmail.com>
2022-10-10 06:55:46 +0300

    category and Todo models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,35 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True,
+                          editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Todo(models.Model):
+    STATUS_CHOICES = [
+        ('Completed', 'Completed'),
+        ('Pending', 'Pending'),
+    ]
+    id = models.UUIDField(primary_key=True, unique=True,
+                          editable=False, default=uuid.uuid4)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    scheduled_time = models.DateField()
+    status = models.CharField(
+        max_length=50, blank=True, null=True, choices=STATUS_CHOICES)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
