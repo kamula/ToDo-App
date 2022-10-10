aa6e8a134cf519afe19c6bbad26a80cb16f39272
Kamula <kamulaisaac@gmail.com>
2022-10-10 08:13:59 +0300

    add category url

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.landing_view,name='landing_view'),
     path('home',views.home,name='home_view'),
+    path('categories',views.add_category,name='add_category_view'),
 ]
