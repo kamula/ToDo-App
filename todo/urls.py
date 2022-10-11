1e9230aec37c0e7bd70c9fdab449681260a6160c
Kamula <kamulaisaac@gmail.com>
2022-10-11 10:02:51 +0300

    update & delete urls

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('home', views.home, name='home_view'),
     path('categories', views.add_category, name='add_category_view'),
     path('todos', views.add_todo_task, name='add_todo_task_view'),
+    path('todos/edit/<uuid:id>', views.edit_todo_task, name='edit_todo_task_view'),
+    path('todos/delete/<uuid:id>', views.delete_todo_task, name='delete_todo_task_view'),
+    path('todos/complete/<uuid:id>', views.complete_todo_task, name='complete_todo_task_view'),
 ]
